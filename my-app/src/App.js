05714a349caf915a1b757903e4e9031be7cfea6e
Rona Wang <rona@mit.edu>
2023-03-26 17:23:15 -0400

    built out basic frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+import { TextField, Typography } from "@mui/material";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="h3" style={{ color: "#1565c0", margin: "20px" }}>
+          Write better raps
+        </Typography>
+        <TextField id="filled-basic" label="Word to rhyme" variant="filled" />
       </header>
     </div>
   );
 }
-
-export default App;
