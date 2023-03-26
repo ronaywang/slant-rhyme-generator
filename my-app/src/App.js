9378b0936b82cf66c0bde6be5de5a5f42133bc6a
Rona Wang <rona@mit.edu>
2023-03-26 17:30:28 -0400

    Imported font

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,14 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Typography variant="h3" style={{ color: "#1565c0", margin: "20px" }}>
+        <Typography
+          variant="h3"
+          style={{
+            color: "#1565c0",
+            margin: "20px",
+            fontFamily: "Nanum Pen Script",
+          }}
+        >
           Write better raps
         </Typography>
         <TextField id="filled-basic" label="Word to rhyme" variant="filled" />
